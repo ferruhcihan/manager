abd77b842ad6d231547cd960e10e0bd26f75ef94
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2025-09-15 09:21:28 +0200

    upcoming: support APL installation on LKE-E

--- packages/manager/src/featureFlags.ts
+++ packages/manager/src/featureFlags.ts
@@ -142,6 +142,7 @@ export interface Flags {
   apiMaintenance: APIMaintenance;
   apl: boolean;
   aplGeneralAvailability: boolean;
+  aplLkeE: boolean;
   blockStorageEncryption: boolean;
   blockStorageVolumeLimit: boolean;
   cloudManagerDesignUpdatesBanner: DesignUpdatesBannerFlag;
